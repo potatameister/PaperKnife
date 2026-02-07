448b7dc61144d4d01c02f87360937b18c4f4199a
potatameister <180633607+potatameister@users.noreply.github.com>
2026-02-07 06:50:35 +0000

    Deploying to gh-pages from @ potatameister/PaperKnife@4afb977c0cf08d1cc086fd9c64b983a899e1281d 🚀

--- assets/pdf-lib-core-8ofwibqN.js
+++ assets/pdf-lib-core-8ofwibqN.js
@@ -1,4 +1,4 @@
-import{g as El}from"./vendor-ui-hg1L1vqF.js";/*! *****************************************************************************
+import{g as El}from"./vendor-ui-BKhDm7Mt.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
